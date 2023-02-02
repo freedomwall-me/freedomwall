0f9157b2d03c1e51035913a69771ae0891e96d9d
bg117 <76200573+bg117@users.noreply.github.com>
2023-02-02 16:40:31 +0800

    remove web routes

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [IndexController::class, 'home'])->name('home');
-Route::get('/contact', [IndexController::class, 'contact'])->name('contact');
-Route::get('/privacy', [IndexController::class, 'privacy'])->name('privacy');
-Route::get('/profile', [IndexController::class, 'profile'])->name('profile')->middleware('verified');
