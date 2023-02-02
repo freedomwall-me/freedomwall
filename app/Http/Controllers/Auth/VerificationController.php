ab22e84882f98ed77ef74156d241506376fed7d1
bg117 <76200573+bg117@users.noreply.github.com>
2023-02-02 16:34:52 +0800

    override resend and verify methods in controller

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\HttpResponse;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
@@ -20,6 +23,7 @@ class VerificationController extends BaseController
     */
 
     use VerifiesEmails;
+    use HttpResponse;
 
     /**
      * Create a new controller instance.
@@ -34,15 +38,40 @@ class VerificationController extends BaseController
     }
 
     // override the resend method
-    public function resend(\Illuminate\Http\Request $request)
+    public function resend(Request $request)
     {
-        if ($request->user()->hasVerifiedEmail())
-            return redirect($this->redirectPath());
+        if ($request->user()->hasVerifiedEmail()) {
+            return $this->error('Email already verified', 422);
+        }
 
         $request->user()->sendEmailVerificationNotification();
 
-        return response()->json([
-            'message' => 'Verification link sent to the email address'
-        ]);
+        return $this->success(null, 'Verification link sent to your email');
+    }
+
+    // override the verify method
+    public function verify(Request $request)
+    {
+        if (!hash_equals((string)$request->route('id'), (string)$request->user()->getKey())) {
+            return $this->error('Unauthorized verification of email', 401);
+        }
+
+        if (!hash_equals((string)$request->route('hash'), sha1($request->user()->getEmailForVerification()))) {
+            return $this->error('Signature does not match', 401);
+        }
+
+        if ($request->user()->hasVerifiedEmail()) {
+            return $this->success(null, 'Email already verified', 204);
+        }
+
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
+
+        if ($response = $this->verified($request)) {
+            return $response;
+        }
+
+        return $this->success(null, 'Email verified successfully');
     }
 }
