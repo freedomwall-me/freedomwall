cee9ece40e7f8e4c39a3f0dc26e1faa08c1f6a30
bg117 <76200573+bg117@users.noreply.github.com>
2023-02-02 16:33:55 +0800

    use prefixes in api endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Auth\AuthenticationController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\WallController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,28 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthenticationController::class, 'login'])->name('auth.login');
+    Route::post('register', [AuthenticationController::class, 'register'])->name('auth.register');
+
+    // protected
+    // %{
+
+    Route::post('logout', [AuthenticationController::class, 'logout'])->name('auth.logout');
+
+    Route::post('verify/email', [VerificationController::class, 'resend'])->name('auth.verify.email');
+    Route::get('verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('auth.verify');
+
+    // password reset
+    Route::get('reset-password/{hash}', [ResetPasswordController::class, 'reset'])->name('auth.reset-password');
+
+    // }%
 });
 
-Route::post('/auth/login', [AuthenticationController::class, 'login'])->name('auth.login');
-Route::post('/auth/register', [AuthenticationController::class, 'register'])->name('auth.register');
+Route::get('walls', [WallController::class, 'index'])->name('wall.index');
+Route::get('wall/{wall}', [WallController::class, 'show'])->name('wall.show');
 
-Route::get('/walls', [WallController::class, 'index'])->name('wall.index');
-Route::get('/wall/{wall}', [WallController::class, 'show'])->name('wall.show');
-
-Route::get('/auth/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('auth.verify');
-
-Route::post('/auth/verify/email', [VerificationController::class, 'resend'])->name('auth.verify.email');
-
-// password reset
-Route::get('/auth/reset-password/{hash}', [ResetPasswordController::class, 'reset'])->name('auth.reset-password');
-
-Route::post('/auth/logout', [AuthenticationController::class, 'logout'])
-    ->middleware('auth:sanctum')
-    ->name('auth.logout');
-
-Route::apiResource('wall', WallController::class)
-    ->middleware('auth:sanctum')
-    ->middleware('verified')
-    ->only(['store', 'update', 'destroy']);
+Route::apiResource('wall', WallController::class)->middleware('auth:sanctum')->middleware('verified')->only(['store', 'update', 'destroy']);
